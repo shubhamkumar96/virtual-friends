2d2d3cba4984bc3a6070421939a5586d4722e44e
shubhamkumar96 <shubhamkumarranu@gmail.com>
2019-12-10 01:44:28 +0530

    Updates

---
+++ precache-manifest.c6e6ec2e5809fdb07406241b5e816ef8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c660aaef21939749846228c900de325c",
+    "url": "/virtual-friends/index.html"
+  },
+  {
+    "revision": "398c135a784479c797fe",
+    "url": "/virtual-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6c2b851dcd4d1a515d40",
+    "url": "/virtual-friends/static/css/main.1a3cddee.chunk.css"
+  },
+  {
+    "revision": "398c135a784479c797fe",
+    "url": "/virtual-friends/static/js/2.33cbe468.chunk.js"
+  },
+  {
+    "revision": "6c2b851dcd4d1a515d40",
+    "url": "/virtual-friends/static/js/main.d54e28e2.chunk.js"
+  },
+  {
+    "revision": "072de8ca5771c1480aca",
+    "url": "/virtual-friends/static/js/runtime-main.4daf2c49.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/virtual-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
